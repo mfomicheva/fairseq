27283de15e663762faa83196bfdd4a9daec5cf53
m_fomicheva <mari.fomicheva@gmail.com>
2019-09-11 12:13:15 +0200

    Check

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -541,7 +541,7 @@ class EnsembleModel(torch.nn.Module):
     def forward_encoder(self, encoder_input):
         if not self.has_encoder():
             return None
-        return [model.encoder(**encoder_input) for model in self.models]
+        return [model.encoder(**encoder_input, training=True) for model in self.models]
 
     @torch.no_grad()
     def forward_decoder(self, tokens, encoder_outs, temperature=1.):
