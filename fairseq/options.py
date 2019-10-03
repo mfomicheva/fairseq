6cbd0c7f9179ed39f572df804cd93293254b413a
m_fomicheva <mari.fomicheva@gmail.com>
2019-10-03 17:44:21 +0200

    Added this option for generation

--- fairseq/options.py
+++ fairseq/options.py
@@ -492,6 +492,8 @@ def add_generation_args(parser):
                        help='strength of diversity penalty for Diverse Beam Search')
     group.add_argument('--print-alignment', action='store_true',
                        help='if set, uses attention feedback to compute and print alignment to source tokens')
+    group.add_argument('--summarize-softmax', action='store_true',
+                       help='if set, print summarization statistic for softmax distribution')
     # fmt: on
     return group
 
