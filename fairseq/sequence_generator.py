7c5b55aa71fe37a1ce3013cef1e7df51cfafffbb
m_fomicheva <mari.fomicheva@gmail.com>
2019-10-03 11:28:18 +0200

    Print src tokens

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -125,6 +125,7 @@ class SequenceGenerator(object):
             if k != 'prev_output_tokens'
         }
 
+        print('Generating...')
         src_tokens = encoder_input['src_tokens']
         print(src_tokens)
         src_lengths = (src_tokens.ne(self.eos) & src_tokens.ne(self.pad)).long().sum(dim=1)
