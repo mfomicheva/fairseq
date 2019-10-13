de3000bab64a78552040e93d5b572cb90025d348
m_fomicheva <mari.fomicheva@gmail.com>
2019-10-13 17:04:43 +0200

    Handle cases where `padding` is None

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -150,7 +150,10 @@ class SequenceGenerator(object):
             encoder_output = encoder_outs[0]['encoder_out']
             encoder_output = encoder_output.transpose(0, 1)  # B x T x C
             padding = encoder_outs[0]['encoder_padding_mask']
-            lengths = len(padding[1]) - padding.sum(dim=1)
+            if padding is not None:
+                lengths = len(padding[1]) - padding.sum(dim=1)
+            else:
+                lengths = 1
             lengths = lengths.unsqueeze(1)
             lengths = lengths.repeat(1, 512)
             lengths = lengths.type(torch.FloatTensor)
