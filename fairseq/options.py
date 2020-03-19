b2203b5e14b847047ef3ad761f656e791be3872b
m_fomicheva <mari.fomicheva@gmail.com>
2020-03-19 19:32:21 +0100

    Removed seed from options

--- fairseq/options.py
+++ fairseq/options.py
@@ -492,8 +492,6 @@ def add_generation_args(parser):
                        help='strength of diversity penalty for Diverse Beam Search')
     group.add_argument('--print-alignment', action='store_true',
                        help='if set, uses attention feedback to compute and print alignment to source tokens')
-    group.add_argument('--seed', type=int, default=None,
-                       help='Fix seed for stochastic decoding')
     # fmt: on
     return group
 
